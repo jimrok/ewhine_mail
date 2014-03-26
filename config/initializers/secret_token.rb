37e18db209da58c490dde606d1bf58895a287247
vulgarcoder <vulgarcoder@gmail.com>
2014-03-26 19:18:48 +0800

    fix file lost.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+
+EwhineMail::Application.config.secret_token = 'dafeb2939ad56bec7117838c3f22448c067acb1cca676ac3a4ff0488f9045af7bd62391f607d2e4316dd6a2cb9d38d776c2061b05e3eac1a46da9de88e8dcc53'
+
+
