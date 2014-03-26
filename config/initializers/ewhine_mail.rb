8b5eb081161f07a2e00b3b280fcaa80d929f83e3
vulgarcoder <vulgarcoder@gmail.com>
2014-03-26 23:32:00 +0800

    fix

--- config/initializers/ewhine_mail.rb
+++ config/initializers/ewhine_mail.rb
@@ -4,17 +4,3 @@ require 'attachment'
 
 $redis = Redis.new :host => '127.0.0.1', :db => 10 # default port : 6379
 
-if Rails.env.production? then
-  redis_conn = proc {
-    $redis
-  }
-
-  Sidekiq.configure_client do |config|
-    config.redis = ConnectionPool.new(size: 20, &redis_conn)
-  end
-
-  # Sidekiq.configure_server do |config|
-  #   config.redis = ConnectionPool.new(size: 20, &redis_conn)
-  # end
-
-end
