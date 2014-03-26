b7eefad25f3370d42308ee2000878fec232a175b
vulgarcoder <vulgarcoder@gmail.com>
2014-03-26 23:48:12 +0800

    added production config

--- config/initializers/mail_receiver.rb
+++ config/initializers/mail_receiver.rb
@@ -3,7 +3,7 @@ require 'rufus/scheduler'
 include MailHelper
 scheduler = Rufus::Scheduler.new(:lockfile => ".rufus-scheduler.lock")
 
-scheduler.every("60") do
+scheduler.every "60s", :allow_overlapping => false do
 	receive_new_email
 end
 
